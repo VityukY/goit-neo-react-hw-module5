4a7535869fa244d93eeca8fec812c54da1a6797e
Yuriy Yuriy <you.thunderstruck@gmail.com>
2024-08-18 23:33:48 +0300

    fix

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,10 @@
-import mobieDetails from '../../apis/movie-details-api.js'
 import { Link } from 'react-router-dom';
 export default function MovieList({ movieList }) {
    return <>
       <ul>
          {movieList.map((movie) => {
-            return <Link key={movie.id} to={movie.id}>
-               <li onClick={()=>{mobieDetails(movie.id)}} >
+            return <Link to={`${movie.id}`} key={movie.id} >
+               <li >
                {movie.title}
                <br />
                <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt="" />
